16bf0fdf72da1840c8d722a94c25b1d7333aedb0
Aurora Takemi <98479040+auroraisluna@users.noreply.github.com>
2023-10-29 07:07:12 +0200

    fix build script

--- buildAndRun.sh
+++ buildAndRun.sh
@@ -2,7 +2,7 @@
 
 # Build the project.
 echo "Building Docker image..."
-docker build -t auroraisluna/ail-services-versioning
+docker build -t auroraisluna/ail-services-versioning .
 
 # Run 
 echo "Running Docker image..."
